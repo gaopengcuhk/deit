6dce2a8e30ba66d20ccbaf021c526b6dcb0a4ba3
gaopengcuhk <51525797+gaopengcuhk@users.noreply.github.com>
2021-03-29 19:36:48 +0800

    Update models.py

--- models.py
+++ models.py
@@ -117,8 +117,8 @@ class Block(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-    def forward(self, x, H, W):
-        x = x + self.drop_path(self.attn(self.norm1(x))
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
     
