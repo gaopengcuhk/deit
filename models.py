6d07c7515f3fd788ed8bf59458d5bef9ecb46b5b
gaopengcuhk <51525797+gaopengcuhk@users.noreply.github.com>
2021-04-10 14:03:25 +0800

    Update models.py

--- models.py
+++ models.py
@@ -817,6 +817,20 @@ def depthwise_small(pretrained=False, **kwargs):
     return model
 
 @register_model
+def mix_depthwise_small(pretrained=False, **kwargs):
+    model = MixVisionTransformer(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+@register_model
 def shift_small(pretrained=False, **kwargs):
     model = ShiftblockTransformer(
         patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
