535e4ea2fecf3c232fdbb249f92107710b177cd8
gaopengcuhk <51525797+gaopengcuhk@users.noreply.github.com>
2021-04-16 10:28:00 +0800

    Update models.py

--- models.py
+++ models.py
@@ -827,6 +827,166 @@ class MixVisionTransformer(nn.Module):
            x = x.flatten(2).mean(-1)
         x = self.head(x)
         return x
+
+    
+    
+class MixVisionTransformer_infinity(nn.Module):
+    """ Vision Transformer
+    A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`  -
+        https://arxiv.org/abs/2010.11929
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None, representation_size=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0., hybrid_backbone=None, norm_layer=None):
+        """
+        Args:
+            img_size (int, tuple): input image size
+            patch_size (int, tuple): patch size
+            in_chans (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (int): embedding dimension
+            depth (int): depth of transformer
+            num_heads (int): number of attention heads
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            drop_rate (float): dropout rate
+            attn_drop_rate (float): attention dropout rate
+            drop_path_rate (float): stochastic depth rate
+            hybrid_backbone (nn.Module): CNN backbone to use in-place of PatchEmbed module
+            norm_layer: (nn.Module): normalization layer
+        """
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6) 
+        embed_dim = [64, 128, 320, 512]
+        depth = [3, 4, 8, 3]
+        num_heads = [1, 2, 5, 8]
+        if hybrid_backbone is not None:
+            self.patch_embed = HybridEmbed(
+                hybrid_backbone, img_size=img_size, in_chans=in_chans, embed_dim=embed_dim)
+        else:
+            self.patch_embed1 = PatchEmbed(
+                img_size=img_size, patch_size=4, in_chans=in_chans, embed_dim=embed_dim[0])
+            self.patch_embed2 = PatchEmbed(
+                img_size=img_size // 4, patch_size=2, in_chans=embed_dim[0], embed_dim=embed_dim[1])
+            self.patch_embed3 = PatchEmbed(
+                img_size=img_size // 8, patch_size=2, in_chans=embed_dim[1], embed_dim=embed_dim[2])
+            self.patch_embed4 = PatchEmbed(
+                img_size=img_size // 16, patch_size=2, in_chans=embed_dim[2], embed_dim=embed_dim[3])
+        num_patches1 = self.patch_embed1.num_patches
+        num_patches2 = self.patch_embed2.num_patches
+        num_patches3 = self.patch_embed3.num_patches
+        num_patches4 = self.patch_embed4.num_patches
+
+#        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed1 = nn.Parameter(torch.zeros(1, embed_dim[0], int(math.sqrt(num_patches1)), int(math.sqrt(num_patches1))))
+        self.pos_embed2 = nn.Parameter(torch.zeros(1, embed_dim[1], int(math.sqrt(num_patches2)), int(math.sqrt(num_patches2))))
+        self.pos_embed3 = nn.Parameter(torch.zeros(1, embed_dim[2], int(math.sqrt(num_patches3)), int(math.sqrt(num_patches3))))
+        self.pos_embed4 = nn.Parameter(torch.zeros(1, embed_dim[3], int(math.sqrt(num_patches4)), int(math.sqrt(num_patches4))))       
+        self.pos_drop = nn.Dropout(p=drop_rate)
+        self.mixture =False
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depth))]  # stochastic depth decay rule
+        self.blocks1 = nn.ModuleList([
+            InfinityBlock(
+                dim=embed_dim[0], num_heads=num_heads[0], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[0])])
+        self.blocks2 = nn.ModuleList([
+            InfinityBlock(
+                dim=embed_dim[1], num_heads=num_heads[1], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[1])])
+        self.blocks3 = nn.ModuleList([
+            InfinityBlock(
+                dim=embed_dim[2], num_heads=num_heads[2], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[2])])
+        if self.mixture:
+           self.blocks4 = nn.ModuleList([
+            Block(
+                dim=embed_dim[3], num_heads=16, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[3])])
+           self.norm = norm_layer(embed_dim[-1])
+        else:
+           self.blocks4 = nn.ModuleList([
+            InfinityBlock(
+                dim=embed_dim[3], num_heads=num_heads[3], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[3])])
+           self.norm = nn.BatchNorm2d(embed_dim[-1])
+        # Representation layer
+        if representation_size:
+            self.num_features = representation_size
+            self.pre_logits = nn.Sequential(OrderedDict([
+                ('fc', nn.Linear(embed_dim, representation_size)),
+                ('act', nn.Tanh())
+            ]))
+        else:
+            self.pre_logits = nn.Identity()
+
+        # Classifier head
+        self.head = nn.Linear(embed_dim[-1], num_classes) if num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.pos_embed1, std=.02)
+        trunc_normal_(self.pos_embed2, std=.02)
+        trunc_normal_(self.pos_embed3, std=.02)
+        trunc_normal_(self.pos_embed4, std=.02)
+        
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed1(x) + self.pos_embed1
+        x = self.pos_drop(x)
+        for blk in self.blocks1:
+            x = blk(x)
+        x = self.patch_embed2(x) + self.pos_embed2
+        for blk in self.blocks2:
+            x = blk(x)
+        x = self.patch_embed3(x) + self.pos_embed3
+        for blk in self.blocks3:
+            x = blk(x)
+        x = self.patch_embed4(x) + self.pos_embed4
+        if self.mixture:
+            x = x.flatten(2).transpose(1, 2)
+        for blk in self.blocks4:
+            x = blk(x)
+        x = self.norm(x)
+        x = self.pre_logits(x)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        if self.mixture:
+           x = x.mean(1)
+        else:
+           x = x.flatten(2).mean(-1)
+        x = self.head(x)
+        return x
     
 class DistilledVisionTransformer(VisionTransformer):
     def __init__(self, *args, **kwargs):
@@ -899,6 +1059,20 @@ def depthwise_small(pretrained=False, **kwargs):
 @register_model
 def mix_depthwise_small(pretrained=False, **kwargs):
     model = MixVisionTransformer(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+@register_model
+def mix_depthwise_small_infinity(pretrained=False, **kwargs):
+    model = MixVisionTransformer_infinity(
         patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
